47a0c85819877050923d4638f9d3f9f441c7da09
Park <timpark0807@gmail.com>
2019-07-03 08:22:33 -0700

    adding tests and updating README

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # U.S. Universities Webscraper
 
-### Purpose
-This is a simple script that scrapes the name, location, and ranking of accredited U.S. Universities. 
+### Description
+A script that produces a CSV containing the name, location, and ranking of all accredited U.S. Universities. 
 
-### Inputs 
+
+### Purpose 
+The goal of this project was to improve and display my programming skillset. Through this experience, I learned more about requests, unit testing, BeautifulSoup, Pandas, and many more programming concepts.
+
+
+#### Inputs 
 ```
 url : string -> https://www.4icu.org/us/
 ```
-### Outputs
+#### Outputs
 The script will produce a CSV titled 'output.csv' with the columns below 
 ```
 name    : string - > Name of the University
 ranking : string -> Ranking of the University
 city    : string -> City that the University is located 
-```
+```    
--- tests/test_output.py
+++ tests/test_output.py
@@ -4,33 +4,48 @@ from webscraper import get_table_rows, create_lists, get_csv
 
 class OutputTest(unittest.TestCase):
 
+    """
+        These tests check the functions written in the webscraper.py file
+        We import get_table_rows, create_lists, and get_csv at the top of this test file
+    """
+
     def setUp(self):
+
+        # We will be scraping the URL below that contains a table with the ranking, name, and city of U.S universities
         self.url = "https://www.4icu.org/us/"
 
     def test_table_rows_exist(self):
-        # Check that the GET request returns a valid object
+
+        # Call function
         test = get_table_rows(self.url)
+
+        # Check that the GET request returns a response
         self.assertIsNotNone(test)
 
     def test_create_lists(self):
+
+        # Call functions
         test = get_table_rows(self.url)
         rank, name, city = create_lists(test)
 
-        # Checks that the lists are not empty
+        # Checks that the returned lists are not empty
         self.assertGreater(len(rank), 1)
         self.assertGreater(len(name), 1)
         self.assertGreater(len(city), 1)
 
-        # Since every University has a rank and city, the length of all lists should be the equal
+        # Since every University has a ranking and city, the length of all lists should be the equal
         self.assertEqual(len(rank), len(name))
         self.assertEqual(len(rank), len(city))
 
     def test_pandas_df(self):
+        """"
+            Testing the accuracy of the returned pandas data frame object
+        """
+
+        # Call functions
         test = get_table_rows(self.url)
         rank, name, city = create_lists(test)
         df  = get_csv(rank, name, city)
-
-        # Testing the accuracy of the returned pandas data frame object
 
         # Check City of University
         city_harvard = df.loc[df['Name'] == 'Harvard University']['City'].item()
